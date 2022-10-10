365afd3cf94a8c1711e354a7a12c96ee623cb359
undevcover <ronkeghali@gmail.com>
2022-10-10 13:29:13 +0100

    keep the program running and add an exit strategy. faulty, but works for now

--- cal.c
+++ cal.c
@@ -6,25 +6,34 @@
 int main ()
 {
 	int firstNum, secondNum, output;
-	char operator;
-
-	printf("Enter the operation\n");
-	scanf("%d %c %d", &firstNum, &operator, &secondNum);
-	switch (operator)
+	char operator, abort;
+	while ((abort != 'X') || (abort != 'x'))
 	{
-		case '+':
-			output = firstNum + secondNum;
-			break;
-		case '-':
-			output = firstNum - secondNum;
-			break;
-		case 'x':
-			output = firstNum * secondNum;
-			break;
-		default:
-			printf("add, subtract or multiply, please");
-	}
-	printf("%d", output);
-    return (0);
-}
+		printf("Enter the operation\n");
+		scanf("%d %c %d", &firstNum, &operator, &secondNum);
+		switch (operator)
+		{
+			case '+':
+				output = firstNum + secondNum;
+				break;
+			case '-':
+				output = firstNum - secondNum;
+				break;
+			case 'x':
+				output = firstNum * secondNum;
+				break;
+			default:
+				printf("add, subtract or multiply, please");
+		}
 
+		printf("%d", output);
+		printf("\npress X to abort\n");
+		scanf("%c", &abort);
+		if ((abort == 'X') || (abort == 'x'))
+                {
+                        break;
+              	}
+	}
+
+	return (0);
+}
